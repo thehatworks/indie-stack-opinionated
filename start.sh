17a724dd17fb7a655e1f0efcd9c3a93323d113ad
Mike Haimes <mike@thehatworks.com>
2022-05-25 21:32:08 -0400

    make it so changes to docker can be made if necessary

--- start.sh
+++ start.sh
@@ -5,13 +5,17 @@
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
-# allocate swap space
-fallocate -l 512M /swapfile
-chmod 0600 /swapfile
-mkswap /swapfile
-echo 10 > /proc/sys/vm/swappiness
-swapon /swapfile
-echo 1 > /proc/sys/vm/overcommit_memory
+if [ $# -eq 0 ]; then
+    >&2 echo "Assuming on fly.io because no extra arguments"
+
+    # allocate swap space
+    fallocate -l 512M /swapfile
+    chmod 0600 /swapfile
+    mkswap /swapfile
+    echo 10 > /proc/sys/vm/swappiness
+    swapon /swapfile
+    echo 1 > /proc/sys/vm/overcommit_memory
+fi
 
 npx prisma migrate deploy
 npm run start
