462870c5e372223ddfa8d43da494e0cbc946e992
Michael Haimes <michael.haimes@gmail.com>
2023-06-16 03:59:57 -0400

    always prefer tailwind classes to style attribute

--- app/routes/notes.new.tsx
+++ app/routes/notes.new.tsx
@@ -46,15 +46,7 @@ export default function NewNotePage() {
   }, [actionData]);
 
   return (
-    <Form
-      method="post"
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        gap: 8,
-        width: "100%",
-      }}
-    >
+    <Form method="post" className="flex w-full flex-col gap-2">
       <div>
         <label className="flex w-full flex-col gap-1">
           <span>Title: </span>
